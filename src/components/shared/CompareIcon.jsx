d43b5d18a614254b9f8b766ea7ddf9d2aad5a391
jhimi01 <talukderjhimi@gmail.com>
2024-10-31 11:40:17 +0600

    formated

--- src/components/shared/CompareIcon.jsx
+++ src/components/shared/CompareIcon.jsx
@@ -1,9 +1,9 @@
-import { Indicator } from '@mantine/core'
-import { MdAddToPhotos } from 'react-icons/md'
+import { Indicator } from "@mantine/core";
+import { MdAddToPhotos } from "react-icons/md";
 
 function CompareIcon() {
   return (
-      <div className="fixed bottom-24 z-50  right-6 rounded  bg-[#e52923] text-white  hover:bg-blue-600 transition">
+    <div className="fixed bottom-24 z-50  right-6 rounded  bg-[#e52923] text-white  hover:bg-blue-600 transition">
       <Indicator size={17} color="#333" label="0">
         <button className=" px-2 flex items-center justify-center flex-col  py-2">
           <MdAddToPhotos size={30} />
@@ -11,7 +11,7 @@ function CompareIcon() {
         </button>
       </Indicator>
     </div>
-  )
+  );
 }
 
-export default CompareIcon
+export default CompareIcon;
--- src/components/shared/ScrollToTop.jsx
+++ src/components/shared/ScrollToTop.jsx
@@ -1,7 +1,6 @@
 import { IoIosArrowUp } from "react-icons/io";
 
 const ScrollToTop = () => {
-
   // Scroll to top of the page
   const scrollToTop = () => {
     window.scrollTo({
@@ -12,17 +11,14 @@ const ScrollToTop = () => {
 
   return (
     <div>
-      
-        <button
-          onClick={scrollToTop}
-          className="fixed bottom-5 z-50  left-6 px-2 rounded-md py-1 bg-white text-primary  hover:bg-blue-600 transition"
-        >
-          <IoIosArrowUp size={30} />
-        </button>
-      
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-5 z-50  left-6 px-2 rounded-md py-1 bg-white text-primary  hover:bg-blue-600 transition"
+      >
+        <IoIosArrowUp size={30} />
+      </button>
     </div>
   );
 };
 
-
-export default ScrollToTop
+export default ScrollToTop;
